dca18ca18f3948636968edd19d165c6ec65d9f5e
Not TCA <realtcatech@gmail.com>
2022-09-20 09:34:31 +0800

    added button to goose command

--- slashCommands/fun/goose.js
+++ slashCommands/fun/goose.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 const request = require("request");
 
 module.exports = {
@@ -23,6 +29,45 @@ module.exports = {
             .setImage(response.request.href)
             .setColor(client.config.color),
         ],
+        components: [
+          new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+              .setCustomId("nextimg")
+              .setLabel("Another one")
+              .setStyle(ButtonStyle.Success)
+          ),
+        ],
+      });
+    });
+
+    const collector = interaction.channel.createMessageComponentCollector({
+      componentType: 2,
+      filter: (i) =>
+        i.user.id === interaction.member.id && i.customId === "nextimg",
+    });
+
+    collector.on("collect", (i) => {
+      request(options, function (error, response, responseBody) {
+        i.update({
+          embeds: [
+            new EmbedBuilder()
+              .setAuthor({
+                name: "*honk*",
+                iconURL:
+                  "https://media.discordapp.net/attachments/978525658449862676/1005311836754825216/goose_emoji.png",
+              })
+              .setImage(response.request.href)
+              .setColor(client.config.color),
+          ],
+          components: [
+            new ActionRowBuilder().addComponents(
+              new ButtonBuilder()
+                .setCustomId("nextimg")
+                .setLabel("Another one")
+                .setStyle(ButtonStyle.Success)
+            ),
+          ],
+        });
       });
     });
   },
